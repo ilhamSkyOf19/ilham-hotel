6dad35ded1ebd7fdb7943f0a2a23efc4f9f8534f
ilham rohmatulloh <ilhamm778866@gmail.com>
2025-12-06 23:15:37 +0700

    mengawali pembuatan Home Page

---
+++ client/src/pages/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import { type FC } from "react";
+
+const HomePage: FC = () => {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+export default HomePage;
